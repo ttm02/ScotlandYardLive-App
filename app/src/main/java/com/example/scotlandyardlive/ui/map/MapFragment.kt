264c0b0237f880bce14ae8004d009a0c2e7a78ce
Tim Jammer <tim.jammer@tu-darmstadt.de>
2023-06-07 23:13:05 +0200

    added marker for current location onto map

--- app/src/main/java/com/example/scotlandyardlive/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/scotlandyardlive/ui/map/MapFragment.kt
@@ -1,21 +1,39 @@
 package com.example.scotlandyardlive.ui.map
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
+import android.location.Location
 import android.os.Bundle
 import android.preference.PreferenceManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import androidx.core.graphics.scale
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.scotlandyardlive.databinding.FragmentHomeBinding
+import com.example.scotlandyardlive.R
 import com.example.scotlandyardlive.databinding.FragmentMapBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.CancellationToken
+import com.google.android.gms.tasks.CancellationTokenSource
+import com.google.android.gms.tasks.OnTokenCanceledListener
 import org.osmdroid.config.Configuration.*
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Marker
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
+import org.osmdroid.views.overlay.mylocation.SimpleLocationOverlay
+import kotlin.math.roundToInt
+
 
 class MapFragment : Fragment() {
 
@@ -28,6 +46,15 @@ class MapFragment : Fragment() {
     private val REQUEST_PERMISSIONS_REQUEST_CODE = 1
     private lateinit var thiscontext: Context
     private lateinit var map : MapView
+
+    //TODO do we need to use only one  Location client?
+    // FusedLocationProviderClient - Main class for receiving location updates.
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    // Start Location: Frankfurt Hauptbahnhof
+    var pos_x: Double = 8.66243955604308 // lon
+    var pos_y: Double = 50.10688202021955 // lat
+
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -54,13 +81,74 @@ class MapFragment : Fragment() {
         map.setTilesScaledToDpi(true)
         map.setMultiTouchControls(true)
 
+
+        var position_marker= Marker(map)
+
+
+        var loc_icon =resources.getDrawable(R.drawable.baseline_location_on_24)
+        position_marker.icon = loc_icon
+        position_marker.position=GeoPoint(pos_y, pos_x)
+        position_marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+        position_marker.setVisible(true)
+        map.overlays.add(position_marker);
+
+
+
+
+
+
         val mapController = map.controller
         mapController.setZoom(14.0)
         // Start Location: Frankfurt Hauptbahnhof
-        val startPoint = GeoPoint(50.10688202021955, 8.66243955604308);
+        val startPoint = GeoPoint(pos_y, pos_x);
         mapController.setCenter(startPoint);
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(thiscontext)
+        if (ActivityCompat.checkSelfPermission(
+                thiscontext,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                MapFragment.LOCATION_PERMISSION_REQUEST_CODE
+            )
+        } else {
+            requestLocation()
+        }
+
         return root
+    }
+
+    private fun requestLocation() {
+
+        try {
+            fusedLocationClient.getCurrentLocation(
+                LocationRequest.PRIORITY_HIGH_ACCURACY,
+                object : CancellationToken() {
+                    override fun onCanceledRequested(p0: OnTokenCanceledListener) =
+                        CancellationTokenSource().token
+
+                    override fun isCancellationRequested() = false
+                })
+                .addOnSuccessListener { location: Location? ->
+                    location?.let {
+
+                        if (location != null) {
+                            // Handle location here
+                            pos_y = it.latitude
+                            pos_x = it.longitude
+                            //TODO update map?
+                        }else
+                            Toast.makeText(thiscontext, "cannot get Location", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+        }
+        catch (s: SecurityException){
+            Toast.makeText(thiscontext, "no permission to get location", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onResume() {
@@ -86,5 +174,9 @@ class MapFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 100
     }
 }
