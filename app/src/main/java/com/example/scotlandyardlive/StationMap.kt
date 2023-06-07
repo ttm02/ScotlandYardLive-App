ee24e83011842ff0bce854d3db23eb091b7fb830
Tim Jammer <tim.jammer@tu-darmstadt.de>
2023-06-07 23:28:33 +0200

    fix wrong disance calculation

--- app/src/main/java/com/example/scotlandyardlive/StationMap.kt
+++ app/src/main/java/com/example/scotlandyardlive/StationMap.kt
@@ -2,10 +2,8 @@ package com.example.scotlandyardlive
 
 import android.app.AlertDialog
 import android.content.Context
-
+import android.util.Log
 import dmax.dialog.SpotsDialog
-import java.lang.Thread.sleep
-import kotlin.math.abs
 import kotlin.math.cos
 import kotlin.math.roundToInt
 import kotlin.math.sqrt
@@ -81,14 +79,40 @@ class StationMap private constructor(
 
         }
 
+        // Debugging
+        //Log.d("get_nearest_stations","Position: ${pos}")
+        //Log.d("get_nearest_stations","${current_best[0]}")
+
         return current_best.mapNotNull { (_, dist, string) -> Pair(string,dist.roundToInt()) }
     }
 
-    fun distance(a: Pair<Double, Double>, b: Pair<Double, Double>): Double {
 
-        val y_diff = (a.second - b.second) / 111320
-        val x_diff = (a.first - b.first) / 40075000 * cos(a.second) / 360
-        return sqrt(y_diff * y_diff + x_diff * x_diff)
+    private fun toRad(value: Double): Double {
+        return value * Math.PI / 180
+    }
+    //HaversineDistance
+    fun distance(a: Pair<Double, Double>, b: Pair<Double, Double>): Double {
+        val R = 6371000; // Radious of the earth in meters
+
+
+        //val y_diff = (a.second - b.second) / 111320
+        //val x_diff = (a.first - b.first) / 40075000 * cos(a.second) / 360
+        //return sqrt(y_diff * y_diff + x_diff * x_diff)
+        val lat1 = a.second
+        val lat2 = b.second
+        val lon1 = a.first
+        val lon2 = b.first
+
+        val latDistance = toRad(lat2 - lat1)
+        val lonDistance = toRad(lon2 - lon1)
+        val a =
+            Math.sin(latDistance / 2) * Math.sin(latDistance / 2) + Math.cos(toRad(lat1)) * Math.cos(
+                toRad(lat2)
+            ) *
+                    Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2)
+        val c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+        val distance = R * c
+        return distance
 
     }
 
