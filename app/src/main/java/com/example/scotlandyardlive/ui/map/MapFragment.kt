eb3a66b8c47cc9e8d95f2c75f3ef277928b6849c
Tim Jammer <tim.jammer@tu-darmstadt.de>
2023-06-17 15:17:53 +0200

    cleaned code and removed some seprecated function calls

--- app/src/main/java/com/example/scotlandyardlive/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/scotlandyardlive/ui/map/MapFragment.kt
@@ -1,49 +1,28 @@
 package com.example.scotlandyardlive.ui.map
 
-import android.Manifest
+//import androidx.lifecycle.ViewModelProvider
+
 import android.content.Context
-import android.content.pm.PackageManager
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.Color
 import android.graphics.PorterDuff
-import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import android.graphics.drawable.LayerDrawable
-import android.graphics.drawable.ScaleDrawable
-import android.location.Location
 import android.os.Bundle
-import android.preference.PreferenceManager
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.core.app.ActivityCompat
-import androidx.core.graphics.scale
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.scotlandyardlive.R
 import com.example.scotlandyardlive.StationMap
 import com.example.scotlandyardlive.TeamPositionsManager
 import com.example.scotlandyardlive.databinding.FragmentMapBinding
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.tasks.CancellationToken
-import com.google.android.gms.tasks.CancellationTokenSource
-import com.google.android.gms.tasks.OnTokenCanceledListener
-import org.osmdroid.api.IGeoPoint
 import org.osmdroid.api.IMapController
-import org.osmdroid.config.Configuration.*
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.Marker
-import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
-import org.osmdroid.views.overlay.mylocation.SimpleLocationOverlay
 import java.time.format.DateTimeFormatter
-import kotlin.math.roundToInt
 
 
 class MapFragment : Fragment() {
@@ -54,13 +33,12 @@ class MapFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
-    private val REQUEST_PERMISSIONS_REQUEST_CODE = 1
     private lateinit var thiscontext: Context
     private lateinit var map: MapView
 
     // Start Location: Frankfurt Hauptbahnhof
-    var pos_x: Double = 8.66243955604308 // lon
-    var pos_y: Double = 50.10688202021955 // lat
+    private var pos_x: Double = 8.66243955604308 // lon
+    private var pos_y: Double = 50.10688202021955 // lat
 
     private lateinit var mapController: IMapController
 
@@ -68,7 +46,7 @@ class MapFragment : Fragment() {
     private lateinit var stationmap: StationMap
     private lateinit var marker_team_X: Marker
     private lateinit var marker_team_rot: Marker
-    private lateinit var marker_team_grün: Marker
+    private lateinit var marker_team_gruen: Marker
     private lateinit var marker_team_gelb: Marker
     private lateinit var marker_team_blau: Marker
     private lateinit var marker_team_orange: Marker
@@ -79,8 +57,8 @@ class MapFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val mapViewModel =
-            ViewModelProvider(this).get(MapViewModel::class.java)
+        //  val mapViewModel =
+        //      ViewModelProvider(this).get(MapViewModel::class.java)
 
         _binding = FragmentMapBinding.inflate(inflater, container, false)
         val root: View = binding.root
@@ -90,23 +68,17 @@ class MapFragment : Fragment() {
         teamManager = TeamPositionsManager.getInstance()
         stationmap = StationMap.getInstance(requireContext())
 
-
-        getInstance().load(
-            requireContext(),
-            PreferenceManager.getDefaultSharedPreferences(requireContext())
-        )
-
         map = binding.map
 
         map.setTileSource(TileSourceFactory.MAPNIK)
 
-        map.setTilesScaledToDpi(true)
+        map.isTilesScaledToDpi = true
         map.setMultiTouchControls(true)
 
 
         marker_team_X = Marker(map)
         marker_team_rot = Marker(map)
-        marker_team_grün = Marker(map)
+        marker_team_gruen = Marker(map)
         marker_team_gelb = Marker(map)
         marker_team_blau = Marker(map)
         marker_team_orange = Marker(map)
@@ -114,7 +86,7 @@ class MapFragment : Fragment() {
         // Add the markers to the map overlays
         map.overlays.add(marker_team_X)
         map.overlays.add(marker_team_rot)
-        map.overlays.add(marker_team_grün)
+        map.overlays.add(marker_team_gruen)
         map.overlays.add(marker_team_gelb)
         map.overlays.add(marker_team_blau)
         map.overlays.add(marker_team_orange)
@@ -122,8 +94,8 @@ class MapFragment : Fragment() {
         mapController = map.controller
         mapController.setZoom(14.0)
         // Start Location: Frankfurt Hauptbahnhof
-        val startPoint = GeoPoint(pos_y, pos_x);
-        mapController.setCenter(startPoint);
+        val startPoint = GeoPoint(pos_y, pos_x)
+        mapController.setCenter(startPoint)
 
         return root
     }
@@ -150,39 +122,39 @@ class MapFragment : Fragment() {
 
     }
 
-    fun update_position_marker(teamname: String) {
-        var color = resources.getColor(R.color.black)
+    private fun update_position_marker(teamname: String) {
+        var color = ContextCompat.getColor(requireContext(), R.color.black)
         var marker = marker_team_X
 
 
         when (teamname) {
             "X" -> {
-                color = resources.getColor(R.color.black)
+                color = ContextCompat.getColor(requireContext(), R.color.black)
                 marker = marker_team_X
             }
 
             "Rot" -> {
-                color = resources.getColor(R.color.red)
+                color = ContextCompat.getColor(requireContext(), R.color.red)
                 marker = marker_team_rot
             }
 
             "Grün" -> {
-                color = resources.getColor(R.color.green)
-                marker = marker_team_grün
+                color = ContextCompat.getColor(requireContext(), R.color.green)
+                marker = marker_team_gruen
             }
 
             "Blau" -> {
-                color = resources.getColor(R.color.blue)
+                color = ContextCompat.getColor(requireContext(), R.color.blue)
                 marker = marker_team_blau
             }
 
             "Gelb" -> {
-                color = resources.getColor(R.color.yellow)
+                color = ContextCompat.getColor(requireContext(), R.color.yellow)
                 marker = marker_team_gelb
             }
 
             "Orange" -> {
-                resources.getColor(R.color.orange)
+                color = ContextCompat.getColor(requireContext(), R.color.orange)
                 marker = marker_team_orange
             }
         }
@@ -194,27 +166,27 @@ class MapFragment : Fragment() {
         } else {
             val last_pos = positions.last()
 
-            var vecicle_icon = resources.getDrawable(R.drawable.b_pic)
+            var vecicle_icon = ContextCompat.getDrawable(requireContext(), R.drawable.b_pic)!!
 
             when (last_pos.Transport) {
                 "R" -> {
-                    vecicle_icon = resources.getDrawable(R.drawable.r_pic)
+                    vecicle_icon = ContextCompat.getDrawable(requireContext(), R.drawable.r_pic)!!
                 }
 
                 "S" -> {
-                    vecicle_icon = resources.getDrawable(R.drawable.s_pic)
+                    vecicle_icon = ContextCompat.getDrawable(requireContext(), R.drawable.s_pic)!!
                 }
 
                 "U" -> {
-                    vecicle_icon = resources.getDrawable(R.drawable.u_pic)
+                    vecicle_icon = ContextCompat.getDrawable(requireContext(), R.drawable.u_pic)!!
                 }
 
                 "T" -> {
-                    vecicle_icon = resources.getDrawable(R.drawable.t_pic)
+                    vecicle_icon = ContextCompat.getDrawable(requireContext(), R.drawable.t_pic)!!
                 }
 
                 "B" -> {
-                    vecicle_icon = resources.getDrawable(R.drawable.b_pic)
+                    vecicle_icon = ContextCompat.getDrawable(requireContext(), R.drawable.b_pic)!!
                 }
             }
 
@@ -250,11 +222,11 @@ class MapFragment : Fragment() {
 
     }
 
-    fun get_position_marker(color: Int, vehicle_icon: Drawable): Drawable {
-        var loc_icon = resources.getDrawable(R.drawable.baseline_location_on_24)
-        val mutableDrawable = loc_icon?.mutate()
-        mutableDrawable?.setColorFilter(color, PorterDuff.Mode.SRC_IN)
-
+    private fun get_position_marker(color: Int, vehicle_icon: Drawable): Drawable {
+        val loc_icon =
+            ContextCompat.getDrawable(requireContext(), R.drawable.baseline_location_on_24)!!
+        val mutableDrawable = loc_icon.mutate()
+        mutableDrawable.setColorFilter(color, PorterDuff.Mode.SRC_IN)
 
         // Create an array of drawables for the layers
         val layers = arrayOf(mutableDrawable, vehicle_icon)
